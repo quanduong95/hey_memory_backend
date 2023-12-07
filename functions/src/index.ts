5ddc88f9688bc105e688ef557db9ec74acb6b7f5
Quan <quan.dn95@gmail.com>
2023-12-06 19:36:39 -0800

    update

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,25 +1,21 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * import {onCall} from "firebase-functions/v2/https";
- * import {onDocumentWritten} from "firebase-functions/v2/firestore";
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-import {onRequest} from "firebase-functions/v2/https";
-import {initializeApp} from "firebase-admin/app";
-import {getFirestore} from "firebase-admin/firestore";
+const { logger } = require('firebase-functions');
+const { onRequest } = require('firebase-functions/v2/https');
+const { onDocumentCreated } = require('firebase-functions/v2/firestore');
+const { initializeApp } = require('firebase-admin/app');
+const { getFirestore } = require('firebase-admin/firestore');
 initializeApp();
-// Start writing functions
-// https://firebase.google.com/docs/functions/typescript
-import {Request, Response} from "express";
-export const addmessage = onRequest(async (req: Request, res: Response) => {
-  // Grab the text parameter.
-  const original = req.query.text;
-  // Push the new message into Firestore using the Firebase Admin SDK.
-  const writeResult = await getFirestore()
-    .collection("messages")
-    .add({original: original});
-  // Send back a message that we"ve successfully written the message
-  res.json({result: `Message with ID: ${writeResult.id} added.`});
+const db = getFirestore();
+
+exports.addmessage = onRequest(async (req: any, res: any) => {
+  const writeResult = await getFirestore().collection('messages').doc().get();
+  res.send(writeResult);
+});
+exports.getAll = onRequest(async (req: any, res: any) => {
+  const citiesRef = db.collection('messages');
+  const snapshot = await citiesRef.get();
+  const array: any = [];
+  snapshot.forEach((doc: any) => {
+    array.push(doc);
+  });
+  res.send(array);
 });
