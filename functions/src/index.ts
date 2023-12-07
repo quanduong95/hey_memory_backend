bc50d3b38609682bc370df64f0ee1e47ce176c74
Quan Duong <quan.dn95@gmail.com>
2023-12-07 11:16:36 -0800

    added a few basic routes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,21 +1,52 @@
-const { logger } = require('firebase-functions');
+import { Response, Request } from 'express';
 const { onRequest } = require('firebase-functions/v2/https');
-const { onDocumentCreated } = require('firebase-functions/v2/firestore');
 const { initializeApp } = require('firebase-admin/app');
 const { getFirestore } = require('firebase-admin/firestore');
+const express = require('express');
+
+const app = express();
 initializeApp();
 const db = getFirestore();
 
-exports.addmessage = onRequest(async (req: any, res: any) => {
-  const writeResult = await getFirestore().collection('messages').doc().get();
-  res.send(writeResult);
+app.get('/messages', async (req: Request, res: Response) => {
+  try {
+    const citiesRef = db.collection('messages');
+    const snapshot = await citiesRef.get();
+    const array: any = [];
+    snapshot.forEach((doc: any) => {
+      array.push(doc._fieldsProto.name);
+    });
+    res.status(200).send(array);
+  } catch (error) {
+    res.status(500).send('Internal error');
+  }
 });
-exports.getAll = onRequest(async (req: any, res: any) => {
-  const citiesRef = db.collection('messages');
-  const snapshot = await citiesRef.get();
-  const array: any = [];
-  snapshot.forEach((doc: any) => {
-    array.push(doc);
-  });
-  res.send(array);
+
+app.put('/message/:id', async (req: Request, res: Response) => {
+  try {
+    console.log(req.params.id);
+    const messageRef = db.collection('messages').doc(req.params.id);
+    await messageRef.set(
+      {
+        capital: true,
+      },
+      { merge: true }
+    );
+    res.status(200).send('Update message successfully');
+  } catch (error) {
+    res.status(500).send('Error updating message');
+  }
 });
+
+app.post('/message', async (req: Request, res: Response) => {
+  try {
+    const data = JSON.parse(req.body);
+    await db.collection('messages').add(data);
+    res.status(200).send('Added data successfully');
+  } catch (error) {
+    res.status(500).send('Internal error');
+    console.log(error);
+  }
+});
+
+exports.routes = onRequest(app);
